53ffaba5c865bc7b7cf0df218d251d7b35cedafa
Vasyl <vasiliy.azarov.vl1@gmail.com>
2025-11-06 15:28:53 +0100

    first commit

---
+++ src/main/java/com/example/vanotificator/VanotificatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.vanotificator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanotificatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanotificatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vanotificator/VanotificatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vanotificator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanotificatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
